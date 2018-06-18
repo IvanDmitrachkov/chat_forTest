031cb1d58a3d2af04c642a707e753c7cf9a5d99a
IvanDmitrachkov <dmitrachkovivan@gmail.com>
2018-06-18 15:25:46 +0300

    вариант без стилей?

---
+++ chat_forTest/Models/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(chat_forTest.Models.Startup))]
+
+namespace chat_forTest.Models
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ chat_forTest/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chat_forTest.Models
+{
+    public class User
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+    }
+}
